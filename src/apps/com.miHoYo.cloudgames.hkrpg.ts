a45b39d844cf73065660c019354d1264e14b4d8f
cjy0812 <cjy081219@Gmail.com>
2026-03-28 02:12:22 +0800

    refactor: 云·星穹铁道 开屏广告-自动下载版本更新
    # perf
    - 全屏广告-自动更新游戏 => 开屏广告-自动下载版本更新

--- src/apps/com.miHoYo.cloudgames.hkrpg.ts
+++ src/apps/com.miHoYo.cloudgames.hkrpg.ts
@@ -5,6 +5,34 @@ export default defineGkdApp({
   name: '云·星穹铁道',
   groups: [
     {
+      key: 0,
+      name: '开屏广告-自动下载版本更新',
+      desc: '强制更新否则不让玩(本质和开屏广告性质一样)',
+      matchTime: 16000,
+      actionMaximum: 2,
+      resetMatch: 'app',
+      fastQuery: true,
+      activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击[立即更新]',
+          matches:
+            '[vid="mUpgradeTitle"][visibleToUser=true] +3 LinearLayout > [vid="mUpgradeDialogOK"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26311879',
+          exampleUrls: 'https://e.gkd.li/eda9555c-565a-4c75-844d-3529d552e203',
+        },
+        {
+          preKeys: [0],
+          name: '点击[开始安装]',
+          matches:
+            'ViewGroup[childCount=3] > [vid="mDescription"][visibleToUser=true] + [vid="mBtnConfirm"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26311880',
+          exampleUrls: 'https://e.gkd.li/752b4fcb-f86a-4a69-b29b-17eaa7c62763',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '功能类-自动点击[使用流量进行游戏]',
       rules: [
@@ -15,20 +43,6 @@ export default defineGkdApp({
           exampleUrls:
             'https://m.gkd.li/57941037/f33b53ea-32a2-48b3-a4a8-54bfa8b1fdd8',
           snapshotUrls: 'https://i.gkd.li/i/14784161',
-        },
-      ],
-    },
-    {
-      key: 2,
-      name: '全屏广告-自动更新游戏', // 我知道这是错误归类，这软件就是必须强制最新版，不然玩不了...
-      desc: '开屏弹窗更新自动确定',
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
-          matches:
-            '[vid="mUpgradeDialogOK"][text="立即更新"][clickable=true][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/25571790',
         },
       ],
     },
